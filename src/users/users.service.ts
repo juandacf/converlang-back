949f1ec460c39fd9a692904f38f7e4ef7cd93d8a
Neil Maldonado <neil.maldonado18@gmail.com>
2026-02-03 21:41:02 -0500

    remover bank id de los servicios

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -17,11 +17,11 @@ export class UsersService {
   }
 
   async getAll(): Promise<User[]> {
-    return this.db.query<User>('SELECT id_user, first_name, last_name, email, gender_id, birth_date, country_id, profile_photo, native_lang_id, target_lang_id, match_quantity, bank_id, role_code, description, is_active, email_verified, last_login created_at, updated_at from get_all_users(TRUE)');
+    return this.db.query<User>('SELECT id_user, first_name, last_name, email, gender_id, birth_date, country_id, profile_photo, native_lang_id, target_lang_id, match_quantity, role_code, description, is_active, email_verified, last_login created_at, updated_at from get_all_users(TRUE)');
   }
 
   async findOne(id_user: Number): Promise<User | null> {
-    const result = await this.db.query<User>('SELECT id_user, first_name, last_name, email, gender_id, birth_date, country_id, profile_photo, native_lang_id, target_lang_id, match_quantity, bank_id, role_code, description, is_active, email_verified, last_login, created_at, updated_at from get_user_by_id($1)', [id_user])
+    const result = await this.db.query<User>('SELECT id_user, first_name, last_name, email, gender_id, birth_date, country_id, profile_photo, native_lang_id, target_lang_id, match_quantity, role_code, description, is_active, email_verified, last_login, created_at, updated_at from get_user_by_id($1)', [id_user])
     return result[0] || null
   }
 
@@ -30,7 +30,7 @@ export class UsersService {
     const passwordHash = await bcrypt.hash(user.password, salt);
 
     const result = await this.db.query<User>(
-      'SELECT * FROM fun_insert_usuarios($1, $2, $3, $4, $5, $6, $7, NULL, $8, $9, 0, NULL, $10, $11)',
+      'SELECT * FROM fun_insert_usuarios($1, $2, $3, $4, $5, $6, $7, NULL, $8, $9, 0, $10, $11)',
       [
         user.first_name,
         user.last_name,
@@ -54,7 +54,7 @@ export class UsersService {
     const result = await this.db.query<{ update_user: string }>(
       `SELECT update_user(
           $1, $2, $3, $4, $5, $6, $7, 
-          $8, $9, $10, $11, $12, $13
+          $8, $9, $10, $11, $12
        ) AS update_user`,
       [
         id_user,
@@ -68,7 +68,6 @@ export class UsersService {
         user.native_lang_id,
         user.target_lang_id,
         user.match_quantity,
-        user.bank_id || null,
         user.description || null,
       ]
     );
