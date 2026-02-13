ffcc6eb8a0d623fda37973969e74e5610bb4a206
Neil Maldonado <neil.maldonado18@gmail.com>
2026-02-12 21:03:03 -0500

    fwefwe

--- src/languages/languages.service.ts
+++ src/languages/languages.service.ts
@@ -5,24 +5,24 @@ import { CreateLanguageDto } from './DTO/create-language.dto';
 
 @Injectable()
 export class LanguagesService {
-    constructor (private readonly db:DatabaseService){
-        
+    constructor(private readonly db: DatabaseService) {
+
     }
 
     async getAll(): Promise<Language[]> {
         return this.db.query<Language>('SELECT language_code, language_name from get_all_languages();');
     }
 
-    async findOne(language_code:string): Promise <Language | null> {
-        const result = await this.db.query<Language> ('select language_code, language_name from get_language_by_id($1);', [language_code])
-        return result[0]|| null
+    async findOne(language_code: string): Promise<Language | null> {
+        const result = await this.db.query<Language>('select language_code, language_name from get_language_by_id($1);', [language_code])
+        return result[0] || null
     }
 
-    async create(data: Language) : Promise <Language> {
+    async create(data: Language): Promise<Language> {
         try {
             const result = await this.db.query<Language>(
                 'SELECT language_code, language_name FROM create_language($1, $2)', [data.language_code, data.language_name]
-                
+
             )
             return result[0]
         } catch (error) {
@@ -31,15 +31,15 @@ export class LanguagesService {
         }
     }
 
-    async update(language_code: string, data:CreateLanguageDto):Promise <Language> {
-        const result = await this.db.query<Language> ('SELECT language_code, language_name FROM update_language($1, $2);',[data.language_code, data.language_name ]);
+    async update(language_code: string, data: CreateLanguageDto): Promise<Language> {
+        const result = await this.db.query<Language>('SELECT language_code, language_name FROM update_language($1, $2);', [data.language_code, data.language_name]);
         return result[0]
     }
 
     async delete(language_code: string): Promise<String> {
-       const result =  await this.db.query('SELECT delete_language($1) AS message', [language_code])
+        const result = await this.db.query('SELECT delete_language($1) AS message', [language_code])
 
-       return result[0].message
+        return result[0].message
     }
-    
+
 }
